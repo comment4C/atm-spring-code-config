368f1aa6c240563c61188fead28d95eb69d8261f
phantichchai <phantichchai1998@gmail.com>
2020-09-11 21:48:14 +0700

    first commit

---
+++ src/main/java/th/ac/ku/atm/Main.java
@@ -0,0 +1,22 @@
+package th.ac.ku.atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import th.ac.ku.atm.ATM;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ATMConfig.class);
+
+        ATM atm = context.getBean(ATM.class);
+
+        String name = atm.validateCustomer(2, 2345);
+        System.out.println( name + " has " + atm.getBalance() );
+        atm.withdraw(200);
+        System.out.println( name + " has " + atm.getBalance() );
+
+        AtmUI atmUI = new AtmUI(atm);
+        atmUI.run();
+    }
+}
